8caa0f1144005d54759c8571544461bcdabb8c46
AlexNL <ya.alexnl@yandex.by>
2019-09-24 00:04:47 +0300

    DemoStyle project

---
+++ DemoStyles/DemoStyles/Models/COntact.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoStyles.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ DemoStyles/DemoStyles/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using DemoStyles.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DemoStyles.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+
+        public ObservableCollection<Contact> Contacts { get; set; }
+
+        public Contact SelectedContact { get; set; }
+
+        public async Task UpdateUsers()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+                var users = JsonConvert.DeserializeObject<List<Contact>>(await response.Content.ReadAsStringAsync());
+
+                await Task.Delay(2000);
+                foreach (var item in users)
+                {
+                    Contacts?.Add(item);
+                }
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            Contacts = new ObservableCollection<Contact>
+            {
+                new Contact
+                {
+                    Name = "Alex",
+                    Phone = "+37529 134 51 8124"
+                },
+                new Contact
+                {
+                    Name = "Dimitry",
+                    Phone = "+37529 5 51 81232"
+                },
+                new Contact
+                {
+                    Name = "Natalya",
+                    Phone = "+37529 213 51 3134"
+                },
+                new Contact
+                {
+                    Name = "Sergei",
+                    Phone = "+37529 547 51 8124"
+                }
+            };
+        }
+
+        public void DeleteAll()
+        {
+            Contacts?.Clear();
+        }
+
+        public void DeleteContact()
+        {
+            Contacts?.Remove(SelectedContact);
+        }
+    }
+}
